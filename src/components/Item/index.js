dc92c885a48339c35e2410405cb6a0014ac19383
Megahand <megahand.x@gmail.com>
2016-09-04 16:27:30 -0400

    forgot clearance and indentation

--- src/components/Item/index.js
+++ src/components/Item/index.js
@@ -6,60 +6,60 @@ const currency = n => n.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,');
 
 export const Item = props => (
   <div className="item">
-  <div className="pre-item">
-    {
-      //Sorry for all that shitty code >> cannot find a way to Vars out of Props !
-      // And i never saw that : "export const Item = props => ()" !!
+    <div className="pre-item">
+      {
+        //Sorry for all that shitty code >> cannot find a way to Vars out of Props !
+        // And i never saw that : "export const Item = props => ()" !!
 
-      Boolean(parseInt(props.labels.backorder, 10)) // to check if backorder
-      ? <img className={'backorder'} src={'/img/backorder/bo2.png'} alt={'Backorder'} /> // Image overlay
-      : ''
-    }
-    { // New Arrival
-      Boolean(parseInt(props.labels.arrival, 10)) && !Boolean(parseInt(props.labels.backorder, 10))
-      ? <img src={`/img/newarrival/new-arrival-${Math.ceil(Math.random() * 4)}.gif`} className={'itemLabelImg'} alt="New arrival!" id={props._id}/>
-      : ''
-    }
-    { // New Product
-      Boolean(parseInt(props.labels.new, 10)) && !Boolean(parseInt(props.labels.backorder, 10))
-      ? <img src={`/img/new/new${Math.ceil(Math.random() * 9)}.gif`} className={'itemLabelImg'} alt="New product!" id={props._id}/>
-      : ''
-    }
-    { // New Price
-      Boolean(parseInt(props.labels.price, 10)) && !Boolean(parseInt(props.labels.backorder, 10))
-      ? <img src={`/img/newprice/new-price-${Math.ceil(Math.random() * 4)}.gif`} className={'itemLabelImg'} alt="New price!" id={props._id}/>
-      : ''
-    }
-    <br/>
-    <small style={{
-      color: '#ccc',
-      float: 'left'
-    }}>{props.price || 'no price'}</small>
-    <b>{props.name}</b><br/>
-    <div className="side_item" style={{
-      float: 'right'
-    }}>
-      <Mesure value={props.width}/><br/>
-      <Mesure value={props.height}/><br/>
-      <Mesure value={props.depth}/><br/>
-      <Mass value={props.weight}/><br/>
+        Boolean(parseInt(props.labels.backorder, 10)) // to check if backorder
+        ? <img className={'backorder'} src={'/img/backorder/bo2.png'} alt={'Backorder'} /> // Image overlay
+        : ''
+      }
+      { // New Arrival
+        Boolean(parseInt(props.labels.arrival, 10)) && !Boolean(parseInt(props.labels.backorder, 10))
+        ? <img src={`/img/newarrival/new-arrival-${Math.ceil(Math.random() * 4)}.gif`} className={'itemLabelImg'} alt="New arrival!" id={props._id}/>
+        : ''
+      }
+      { // New Product
+        Boolean(parseInt(props.labels.new, 10)) && !Boolean(parseInt(props.labels.backorder, 10))
+        ? <img src={`/img/new/new${Math.ceil(Math.random() * 9)}.gif`} className={'itemLabelImg'} alt="New product!" id={props._id}/>
+        : ''
+      }
+      { // New Price
+        Boolean(parseInt(props.labels.price, 10)) && !Boolean(parseInt(props.labels.backorder, 10))
+        ? <img src={`/img/newprice/new-price-${Math.ceil(Math.random() * 4)}.gif`} className={'itemLabelImg'} alt="New price!" id={props._id}/>
+        : ''
+      }
+      <br/>
+      <small style={{
+        color: '#ccc',
+        float: 'left'
+      }}>{props.price || 'no price'}</small>
+      <b>{props.name}</b><br/>
+      <div className="side_item" style={{
+        float: 'right'
+      }}>
+        <Mesure value={props.width}/><br/>
+        <Mesure value={props.height}/><br/>
+        <Mesure value={props.depth}/><br/>
+        <Mass value={props.weight}/><br/>
+      </div>
+      <span>
+        <small>
+          {/* {<img className="png_alpha" alt={props.code} src={`//rockplusinc.com/img/full/thumbs/_${props.code}.png`}/>} */}
+          <Details
+            imgClassName={"png_alpha"}
+            src={`//rockplusinc.com/img/full/_${props.code}.png`}
+            thumbsSrc={`//rockplusinc.com/img/full/thumbs/_${props.code}.png`}
+            title={props.name}
+          />
+        </small>
+      </span><br/>
+      2 Per Packs<br/>
+      <span className={Boolean(parseInt(props.labels.clearance, 10)) ? "clearance" : ""}>
+        {currency(props.price)}$
+      </span><br/>
     </div>
-    <span>
-      <small>
-        {/* {<img className="png_alpha" alt={props.code} src={`//rockplusinc.com/img/full/thumbs/_${props.code}.png`}/>} */}
-        <Details
-          imgClassName={"png_alpha"}
-          src={`//rockplusinc.com/img/full/_${props.code}.png`}
-          thumbsSrc={`//rockplusinc.com/img/full/thumbs/_${props.code}.png`}
-          title={props.name}
-        />
-      </small>
-    </span><br/>
-    2 Per Packs<br/>
-    <span className={props.labels.clearance === 1 ? "clearance" : ""}>
-      {currency(props.price)}$
-    </span><br/>
-  </div>
   </div>
 );
 
