f12930f8c568abdec0db0733ec197b502551bef0
Olivier Rousseau <zeachco@gmail.com>
2016-09-20 23:01:36 +0530

    event listener sidebar responsive

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Link} from 'react-router';
 // import store from 'core/store';
 import {browserHistory} from 'react-router';
@@ -8,50 +8,74 @@ const navigate = ev => {
   browserHistory.push(ev.target.value);
 }
 
-const Sidebar = props => {
-  const state = props;
-  const {categories, displayName} = props;
-  const current = props.params && props.params.category;
-  if (window.innerWidth > 800) {
-    return (
-      <div id="sidenav">
-        <ul>
-          Welcome<br/> {displayName}
-          <hr/>
-          <li>
-            <Link to='/' className={!current
-              ? 'current'
-              : ''}>Home</Link>
-          </li>
-          <li>
-            <Link to="/logout">logout</Link>
-          </li>
-          <hr/> {categories.map(cat => (
-            <li key={cat.value}>
-              <Link style={cat.style} className={cat.value === current
-                ? 'current'
-                : ''} to={`/category/${cat.value}`}>{cat.label}</Link>
-            </li>
-          ))}
-        </ul>
-      </div>
-    );
-  } else {
-    return <div className="mobile_categories">
-      <select onChange={navigate} value={current || 'home'}>
-        {[
-          {
-            value: 'home',
-            label: 'Home'
-          },
-          ...state.categories
-        ].map(cat => (
-          <option style={cat.style} key={cat.value} value={cat.value}>{cat.label}</option>
-        ))}
-      </select>
-    </div>
+class Sidebar extends Component {
+  constructor(...args) {
+    super(...args);
+    this.state = {
+      mobile: window.innerWidth <= 800
+    };
+    this.resize = this._resize.bind(this);
   }
-};
+
+  _resize(ev) {
+    this.setState({
+      mobile: window.innerWidth <= 800
+    });
+  }
+
+  componentDidMount() {
+    window.addEventListener('resize', this.resize);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.resize);
+  }
+
+  render() {
+    const {mobile} = this.state;
+    const {categories, displayName} = this.props;
+    const current = this.props.params && this.props.params.category;
+    if (!mobile) {
+      return (
+        <div id="sidenav">
+          <ul>
+            Welcome<br/> {displayName}
+            <hr/>
+            <li>
+              <Link to='/' className={!current
+                ? 'current'
+                : ''}>Home</Link>
+            </li>
+            <li>
+              <Link to="/logout">logout</Link>
+            </li>
+            <hr/> {categories.map(cat => (
+              <li key={cat.value}>
+                <Link style={cat.style} className={cat.value === current
+                  ? 'current'
+                  : ''} to={`/category/${cat.value}`}>{cat.label}</Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      );
+    } else {
+      return <div className="mobile_categories">
+        <select onChange={navigate} value={current || 'home'}>
+          {[
+            {
+              value: 'home',
+              label: 'Home'
+            },
+            ...categories
+          ].map(cat => (
+            <option style={cat.style} key={cat.value} value={cat.value}>{cat.label}</option>
+          ))}
+        </select>
+      </div>
+    }
+  }
+}
 
 const mapStatetoProps = (store, ownProps) => {
   return ({
