47f33b2c75c6d6882f8b6e8fac270a0dcec2d27b
Olivier Rousseau-Guyot <zeachco@gmail.com>
2017-01-12 17:41:57 -0500

    fixed login footer

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,10 +3,11 @@ import {LoginForm} from '../components';
 
 const LoginPage = props => (
   <div id="login">
-    <LoginForm />
+    <LoginForm/>
     <footer>
       <p>
-        If you are looking to become a wholesale customer or for general inquiries please contact us.
+        If you are looking to become a wholesale customer or for general inquiries
+        please contact us.
       </p>
       <p>
         <a className="button-link" href="mailto:info@rockplusinc.com">info@rockplusinc.com</a>
@@ -15,8 +16,7 @@ const LoginPage = props => (
         <a className="button-link" href="tel:1-866-651-6366">international: 1-866-651-6366</a>
       </p>
       <p>
-        1700 Ch. Chambly, Longueuil
-        Qc, Canada, J4J 3X5
+        1700 Ch. Chambly, Longueuil Qc, Canada, J4J 3X5
       </p>
     </footer>
   </div>
