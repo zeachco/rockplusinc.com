11003fb1a45e9ec3296f2e35435dcbb898116a02
Olivier Rousseau-Guyot <zeachco@gmail.com>
2017-03-24 23:30:33 -0400

    added IE 10

--- src/store/actions/categories.js
+++ src/store/actions/categories.js
@@ -2,14 +2,12 @@ import axios from 'axios';
 import store from '..';
 
 export function fetchCategories() {
-  axios
-    .get('/api/categories/rockplusinc.com')
-    .then(function(xhr) {
-      store.dispatch({
-        type: 'FETCH_CATEGORIES_DONE',
-        payload: xhr.data
-      });
+  axios.get('/api/categories/rockplusinc.com').then(xhr => {
+    store.dispatch({
+      type: 'FETCH_CATEGORIES_DONE',
+      payload: xhr.data
     });
+  });
   store.dispatch({
     type: 'FETCH_CATEGORIES_START'
   });
