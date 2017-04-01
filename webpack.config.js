e30b3a975552cd03629ca59b49b2867a3d4dd0e6
Olivier Rousseau-Guyot <zeachco@gmail.com>
2017-04-01 09:35:08 -0400

    webpack changes for deployment

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const wps = require('webpack-production-setup');
 
 module.exports = argConfig => wps(Object.assign({
   favicon: 'src/favicon.png',
-  es6Modules: [/skylight/, /axios/, /cms\-core/],
+  es6Modules: [/skylight/, /axios/, /cms\-core/, /auto\-bind/],
   proxy: {
     '/api/': {
       target: {
