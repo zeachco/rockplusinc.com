827ec9923f98711aafe4459f310b17c87d0e6628
Olivier Rousseau-Guyot <zeachco@gmail.com>
2017-08-01 20:55:08 -0400

    eval source maps

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const wps = require('webpack-production-setup');
 module.exports = argConfig => wps(Object.assign({
   favicon: 'src/favicon.png',
   es6Modules: [/skylight/, /axios/, /cms\-core/, /auto\-bind/, /bulma/],
-  devtool: 'inline-source-map',
+  devtool: 'eval',
   proxy: {
     '/api/': {
       target: 'http://rockplus.com:8080',
